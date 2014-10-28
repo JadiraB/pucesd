fc8d52136acf5bd3ad222edad596a44e9b3c46ba
andresluzu <andresluzu@gmail.com>
2014-10-28 12:41:30 -0500

    Implementacion de Repositorios

--- inves/src/main/java/com/pucesd/inves/actions/Index.java
+++ inves/src/main/java/com/pucesd/inves/actions/Index.java
@@ -1,18 +1,31 @@
 package com.pucesd.inves.actions;
 
+import com.inves.models.TipoProducto;
+import com.inves.repos.TipoProductoRepository;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Results({
         @Result(name = Action.SUCCESS, location = "${redirectName}", type = "redirectAction")
 })
 public class Index extends ActionSupport {
 
+	@Autowired
+	private TipoProductoRepository repo; 
+	
     private String redirectName;
 
     public String execute() {
+    	
+    	Iterable<TipoProducto> tipos = repo.findAll();
+    	for (TipoProducto tipoProducto : tipos) {
+			System.out.println(tipoProducto);
+		}
+    	
         redirectName = "hello";
         return Action.SUCCESS;
     }
